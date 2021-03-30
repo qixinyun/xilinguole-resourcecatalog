5b5cd924a121554708bfd1bb45e44160910e3c16
li_jia <1229844289@qq.com>
2021-03-30 15:20:28 +0800

    v0.3.0

--- src/ResourceCatalog/Model/IResourceCatalogTypeName.php
+++ src/ResourceCatalog/Model/IResourceCatalogTypeName.php
@@ -5,8 +5,8 @@ interface IResourceCatalogTypeName
 {
     const TYPE_NAME = array(
         IResourceCatalog::TYPE['NULL'] => '',
-        IResourceCatalog::TYPE['XBXZCF'] => '2019行政处罚公示信息',
-        IResourceCatalog::TYPE['XBXZXK'] => '2019行政许可公示信息',
+        IResourceCatalog::TYPE['XBXZCF'] => '行政处罚公示信息',
+        IResourceCatalog::TYPE['XBXZXK'] => '行政许可公示信息',
 IResourceCatalog::TYPE['HEIMDFR'] => '黑名单(法人)',
 IResourceCatalog::TYPE['HEIMDZRR'] => '黑名单(自然人)',
 IResourceCatalog::TYPE['HMDFR'] => '红名单(法人)',
